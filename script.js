e0fda22290af82b25879b6b98e98ff269687c785
Lucas Blanger <78960701+Lucas-Blanger@users.noreply.github.com>
2024-09-14 11:46:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const toggleButton = document.getElementById('toggleLanguage');
 toggleButton.addEventListener('click', toggleLanguage);
 
 function toggleLanguage() {
-    const currentPath = window.location.pathname; 
-    
+    const baseUrl = 'https://lucas-blanger..github.io/portfolio/';
+    const currentPath = window.location.pathname;
+
     if (currentPath.includes('index.html')) {
-        window.location.pathname = currentPath.replace('index.html', 'index-en.html');
+        window.location.href = baseUrl + 'index-en.html';
     } else if (currentPath.includes('index-en.html')) {
-        window.location.pathname = currentPath.replace('index-en.html', 'index.html');
+        window.location.href = baseUrl + 'index.html';
     }
 }
