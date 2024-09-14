e7d4583b5391a865b9f32fe72fd17f3cb79a7cbf
Lucas Blanger <78960701+Lucas-Blanger@users.noreply.github.com>
2024-09-14 11:44:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ toggleButton.addEventListener('click', toggleLanguage);
 
 function toggleLanguage() {
     const currentUrl = window.location.href;
-    if (currentUrl.includes('/index.html')) {
-        window.location.href = currentUrl.replace('/index.html', '/index-en.html');
-    } else if (currentUrl.includes('/index-en.html')) {
-        window.location.href = currentUrl.replace('/index-en.html', '/index.html');
+    
+    const filename = currentUrl.substring(currentUrl.lastIndexOf('/') + 1);
+
+    if (filename === 'index.html') {
+        window.location.href = currentUrl.replace('index.html', 'index-en.html');
+    } else if (filename === 'index-en.html') {
+        window.location.href = currentUrl.replace('index-en.html', 'index.html');
     }
 }
